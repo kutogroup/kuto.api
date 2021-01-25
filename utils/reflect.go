e2dc694e01ba52e969f940b0b1b381195abe140a
Simon Sun <sun.zg@outlook.com>
2021-01-25 17:36:26 +0800

    update

--- utils/reflect.go
+++ utils/reflect.go
@@ -1,6 +1,9 @@
 package utils
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 //ReflectGetVTByOrigin 获取反射的type和value，保留指针
 func ReflectGetVTByOrigin(model interface{}) (t reflect.Type, v reflect.Value) {
@@ -31,4 +34,50 @@ func ReflectSetValue(model interface{}, key string, value interface{}) {
 	}
 
 	v.FieldByName(key).Set(reflect.ValueOf(value))
+}
+
+//CopyFields 复制结构体
+func CopyFields(dst interface{}, src interface{}, fields ...string) (err error) {
+	at := reflect.TypeOf(a)
+	av := reflect.ValueOf(a)
+	bt := reflect.TypeOf(b)
+	bv := reflect.ValueOf(b)
+
+	// 简单判断下
+	if at.Kind() != reflect.Ptr {
+		err = fmt.Errorf("a must be a struct pointer")
+		return
+	}
+	av = reflect.ValueOf(av.Interface())
+
+	// 要复制哪些字段
+	_fields := make([]string, 0)
+	if len(fields) > 0 {
+		_fields = fields
+	} else {
+		for i := 0; i < bv.NumField(); i++ {
+			_fields = append(_fields, bt.Field(i).Name)
+		}
+	}
+
+	if len(_fields) == 0 {
+		fmt.Println("no fields to copy")
+		return
+	}
+
+	// 复制
+	for i := 0; i < len(_fields); i++ {
+		name := _fields[i]
+		f := av.Elem().FieldByName(name)
+		bValue := bv.FieldByName(name)
+
+		// a中有同名的字段并且类型一致才复制
+		if f.IsValid() && f.Kind() == bValue.Kind() {
+			f.Set(bValue)
+		} else {
+			fmt.Printf("no such field or different kind, fieldName: %s\n", name)
+		}
+	}
+
+	return
 }
